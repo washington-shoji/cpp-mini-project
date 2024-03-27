a29b89fcdbf5883c333929789a1381f3cd63a8b1
washington-shoji <shoji.washington@gmail.com>
2024-03-27 17:09:32 +1100

    CGPA calculator completed

---
+++ calculatecgpa.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double CgpaCalculator(double marks[], int number)
+{
+    double grade[number];
+
+    double cgpa = 0;
+    double sum = 0;
+
+    for (int i = 0; i < number; i++)
+    {
+        grade[i] = (marks[i] / 10);
+    }
+
+    for (int i = 0; i < number; i++)
+    {
+        sum += grade[i];
+    }
+
+    cgpa = sum / number;
+
+    return cgpa;
+}
+
+int main()
+{
+    int number = 5;
+    double marks[] = {90, 80, 70, 80, 90};
+
+    double cgpa = CgpaCalculator(marks, number);
+
+    cout << "CGPA = ";
+    printf("%.1f\n", cgpa);
+    cout << "CGPA Percentage = ";
+    printf("%.2f%%\n", cgpa * 9.5);
+}
